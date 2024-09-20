0aeac35a3f232f3a7b540eb94fcb96a1cfc80048
Aayush <aayushtimsina19@gmail.com>
2024-09-20 13:43:37 +0530

    Make a class inputembeddings

--- models/model.py
+++ models/model.py
@@ -1,4 +1,11 @@
 import torch
 import torch.nn
 
+class InputEmbeddings(torch.nn.Module):
+    
+    def __init__(self,d_model,vocab_size:int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+
 
