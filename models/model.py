d6bbd273748e4e826b9209755325d4a2908a3898
Aayush <aayushtimsina19@gmail.com>
2024-09-20 13:46:51 +0530

    another function forward

--- models/model.py
+++ models/model.py
@@ -2,10 +2,13 @@ import torch
 import torch.nn
 
 class InputEmbeddings(torch.nn.Module):
-    
+
     def __init__(self,d_model,vocab_size:int):
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
-
+        self.embedding = torch.nn.Embedding(vocab_size,d_model)
+    
+    def forword(self,x):
+        return self.embedding(x)
 
